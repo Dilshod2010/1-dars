701da5fc82719a64d3299dc6ea2c2f205a686bbc
Dilshod2010 <dilshodjon.fahriddinov@gmail.com>
2024-12-24 19:10:05 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+//const num = +prompt("Raqam kiriting")
+//const  num2= +prompt("Ikkinchi raqam kiritng")
+
+//console.log(`Javob : ${num} + ${num2} = ${num + num2}`);
+
+//const username = prompt("raqam kiritng")
+//const useribo = prompt("Ikkinchi raqam")
+//alert  (username + useribo)
+
+const num =prompt("Imtihondan o'tish bali 80 Ball")
+console.log("num");
+
+
+
+
